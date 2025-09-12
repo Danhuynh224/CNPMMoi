baeade64ca7a84a0c1cb99ad3a9c7d776086356f
Danhuynh224 <vdan2242004@gmail.com>
2025-09-12 13:50:22 +0700

    sửa để publish

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
   build: {
     lib: {
-      entry: "src/index.ts",
-      name: "ReactCartLibrary",
+      entry: path.resolve(__dirname, "src/index.ts"), // entry chính
+      name: "ReactCartLibrary", // global name khi dùng UMD
       fileName: (format) => `react-cart-library.${format}.js`,
+      formats: ["es", "cjs", "umd"], // xuất đủ 3 dạng
     },
     rollupOptions: {
-      external: ["react", "react-dom"],
+      external: ["react", "react-dom"], // không bundle react
       output: {
         globals: {
           react: "React",
